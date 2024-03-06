33108b028fd1fbdf1714ec4ca766bcfdaf6f32fb
SK <sun-kyu.kim@aya.yale.edu>
2024-03-06 11:13:03 -0500

    add backart

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ import fs from 'fs';
 const main = () => {
     const baseUrl = 'https://d3alac64utt23z.cloudfront.net/images/cards/SOR/';
 
-    for (let i = 241; i < 253; i++) {
-        let imageName = './cards/' + i.toString().padStart(3, '0') + '.png'
+    for (let i = 1; i < 19; i++) {
+        let imageName = './cards/' + i.toString().padStart(3, '0') + '-b.png'
         let file = fs.createWriteStream(imageName);
-        let url = baseUrl + i.toString().padStart(3, '0') + '.png'
+        let url = baseUrl + i.toString().padStart(3, '0') + '-b.png'
 
         https.get(url, response => {
             response.pipe(file);
